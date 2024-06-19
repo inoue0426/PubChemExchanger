f739e3c141b878398fd70a49fe75dc30b4b806a8
inoue0426 <sekihan_tennis@yahoo.co.jp>
2024-06-19 12:33:00 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# python_template
+# PubChem Exchanger.
+
+## How to use 
+
+```python
+PubChemExchanger on  main [?] via 🅒 multi 
+➜ python pubchemExchanger.py -d camptothecin
+                                                         SMILES      CID SID              NSC     DBID
+camptothecin  CCC1(C2=C(COC1=O)C(=O)N3CC4=CC5=CC=CC=C5N=C4C3...  [24360]  []  [100880, 94600]  DB04690
+(multi) 
+PubChemExchanger on  main [?] via 🅒 multi 
+➜ python pubchemExchanger.py -d topotecan   
+                                                      SMILES      CID SID               NSC     DBID
+topotecan  CCC1(C2=C(COC1=O)C(=O)N3CC4=CC5=C(C=CC(=C5CN(C...  [60700]  []  [609699, 641007]  DB01030
+(multi) 
+PubChemExchanger on  main [?] via 🅒 multi 
+➜ 
+```
---
+++ pubchemExchanger.py
@@ -0,0 +1,47 @@
+import requests
+import argparse
+import pandas as pd
+
+def fetch_data(url):
+    response = requests.get(url)
+    return response.json() if response.status_code == 200 else None
+
+def get_property(data, key, default="Not found"):
+    return data.get(key, default)
+
+def get_identifiers(data, key):
+    return data['IdentifierList'].get(key, []) if data and 'IdentifierList' in data else []
+
+def get_synonyms(data, prefix):
+    return [syn for syn in data['InformationList']['Information'][0]['Synonym'] if syn.startswith(prefix)] if data else []
+
+def get_drug_info(drug_name):
+    data = {
+        'SMILES': "Not found",
+        'CID': [],
+        'SID': [],
+        'NSC': [],
+        'DBID': "Not found"
+    }
+
+    base_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{drug_name}/"
+
+    properties = fetch_data(f"{base_url}property/CanonicalSMILES/JSON")
+    data['SMILES'] = get_property(properties['PropertyTable']['Properties'][0], 'CanonicalSMILES') if properties else "Not found"
+
+    data['CID'] = get_identifiers(fetch_data(f"{base_url}cids/JSON"), 'CID')
+    data['SID'] = get_identifiers(fetch_data(f"{base_url}sids/JSON"), 'SID')
+
+    synonyms = fetch_data(f"{base_url}synonyms/JSON")
+    nsc_synonyms = get_synonyms(synonyms, 'NSC')
+    data['NSC'] = list(set(''.join(filter(str.isdigit, syn)) for syn in nsc_synonyms))
+    data['DBID'] = next((syn for syn in get_synonyms(synonyms, 'DB')), "Not found")
+
+    return pd.DataFrame([data], index=[drug_name])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Retrieve information about a drug from PubChem')
+    parser.add_argument('-d', '--drug_name', type=str, help='The name of the drug to query')
+    args = parser.parse_args()
+    result = get_drug_info(args.drug_name)
+    print(result)
