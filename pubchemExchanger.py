ab06cd1396dc0a26a0f18b0439ee5d4d95746f63
inoue0426 <inoue0426@users.noreply.github.com>
2024-06-19 16:49:18 +0000

    Apply auto-formatting

--- pubchemExchanger.py
+++ pubchemExchanger.py
@@ -1,42 +1,67 @@
 import argparse
+from functools import lru_cache
+
 import pandas as pd
 import requests
-from functools import lru_cache
+
 
 @lru_cache(maxsize=None)
 def fetch_data(url):
     response = requests.get(url)
     return response.json() if response.status_code == 200 else None
 
+
 def get_property(data, key, default="Not found"):
     return data.get(key, default)
+
 
 def get_identifiers(data, key):
     return (
         data["IdentifierList"].get(key, []) if data and "IdentifierList" in data else []
     )
 
+
 def get_synonyms(data):
-    return (
-        data["InformationList"]["Information"][0].get("Synonym", [])
-        if data
-        else []
-    )
+    return data["InformationList"]["Information"][0].get("Synonym", []) if data else []
+
 
 def extract_nsc_numbers(synonyms):
-    return list(set("".join(filter(str.isdigit, syn)) for syn in synonyms if syn.startswith("NSC")))
+    return list(
+        set(
+            "".join(filter(str.isdigit, syn))
+            for syn in synonyms
+            if syn.startswith("NSC")
+        )
+    )
+
 
 def extract_dbid(synonyms):
     return next((syn for syn in synonyms if syn.startswith("DB")), "Not found")
 
+
 def get_drug_info(drug_names):
     drugs_data = []
     for drug_name in drug_names:
-        data = {"SMILES": "Not found", "CID": [], "SID": [], "NSC": [], "DBID": "Not found", "Synonyms": []}
-        base_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{drug_name}/"
+        data = {
+            "SMILES": "Not found",
+            "CID": [],
+            "SID": [],
+            "NSC": [],
+            "DBID": "Not found",
+            "Synonyms": [],
+        }
+        base_url = (
+            f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{drug_name}/"
+        )
 
         properties = fetch_data(f"{base_url}property/CanonicalSMILES/JSON")
-        data["SMILES"] = get_property(properties["PropertyTable"]["Properties"][0], "CanonicalSMILES") if properties else "Not found"
+        data["SMILES"] = (
+            get_property(
+                properties["PropertyTable"]["Properties"][0], "CanonicalSMILES"
+            )
+            if properties
+            else "Not found"
+        )
 
         data["CID"] = get_identifiers(fetch_data(f"{base_url}cids/JSON"), "CID")
         data["SID"] = get_identifiers(fetch_data(f"{base_url}sids/JSON"), "SID")
@@ -50,9 +75,18 @@ def get_drug_info(drug_names):
 
     return pd.DataFrame(drugs_data, index=drug_names)
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Retrieve information about drugs from PubChem")
-    parser.add_argument("-d", "--drug_names", nargs='+', type=str, help="The names of the drugs to query")
+    parser = argparse.ArgumentParser(
+        description="Retrieve information about drugs from PubChem"
+    )
+    parser.add_argument(
+        "-d",
+        "--drug_names",
+        nargs="+",
+        type=str,
+        help="The names of the drugs to query",
+    )
     args = parser.parse_args()
     result = get_drug_info(args.drug_names)
     print(result)
