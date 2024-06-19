51a8b3cf83435ca9afcb0d8c7365681dfd1c333c
inoue0426 <sekihan_tennis@yahoo.co.jp>
2024-06-19 12:48:54 -0400

    working for multiple drugs

--- README.md
+++ README.md
@@ -3,16 +3,11 @@
 ## How to use 
 
 ```python
-PubChemExchanger on  main [?] via 🅒 multi 
-➜ python pubchemExchanger.py -d camptothecin
-                                                         SMILES      CID SID              NSC     DBID
-camptothecin  CCC1(C2=C(COC1=O)C(=O)N3CC4=CC5=CC=CC=C5N=C4C3...  [24360]  []  [100880, 94600]  DB04690
+PubChemExchanger on  main [!?] via 🅒 multi 
+➜ python pubchemExchanger.py -d topotecan camptothecin
+                                                         SMILES      CID SID               NSC     DBID                                           Synonyms
+topotecan     CCC1(C2=C(COC1=O)C(=O)N3CC4=CC5=C(C=CC(=C5CN(C...  [60700]  []  [641007, 609699]  DB01030  [topotecan, 123948-87-8, Topotecan lactone, Hy...
+camptothecin  CCC1(C2=C(COC1=O)C(=O)N3CC4=CC5=CC=CC=C5N=C4C3...  [24360]  []   [94600, 100880]  DB04690  [camptothecin, 7689-03-4, Camptothecine, (S)-(...
 (multi) 
-PubChemExchanger on  main [?] via 🅒 multi 
-➜ python pubchemExchanger.py -d topotecan   
-                                                      SMILES      CID SID               NSC     DBID
-topotecan  CCC1(C2=C(COC1=O)C(=O)N3CC4=CC5=C(C=CC(=C5CN(C...  [60700]  []  [609699, 641007]  DB01030
-(multi) 
-PubChemExchanger on  main [?] via 🅒 multi 
-➜ 
+PubChemExchanger on  main [!?] via 🅒 multi took 2s 
 ```
--- pubchemExchanger.py
+++ pubchemExchanger.py
@@ -1,66 +1,58 @@
 import argparse
-
 import pandas as pd
 import requests
+from functools import lru_cache
 
-
+@lru_cache(maxsize=None)
 def fetch_data(url):
     response = requests.get(url)
     return response.json() if response.status_code == 200 else None
 
-
 def get_property(data, key, default="Not found"):
     return data.get(key, default)
-
 
 def get_identifiers(data, key):
     return (
         data["IdentifierList"].get(key, []) if data and "IdentifierList" in data else []
     )
 
-
-def get_synonyms(data, prefix):
+def get_synonyms(data):
     return (
-        [
-            syn
-            for syn in data["InformationList"]["Information"][0]["Synonym"]
-            if syn.startswith(prefix)
-        ]
+        data["InformationList"]["Information"][0].get("Synonym", [])
         if data
         else []
     )
 
+def extract_nsc_numbers(synonyms):
+    return list(set("".join(filter(str.isdigit, syn)) for syn in synonyms if syn.startswith("NSC")))
 
-def get_drug_info(drug_name):
-    data = {"SMILES": "Not found", "CID": [], "SID": [], "NSC": [], "DBID": "Not found"}
+def extract_dbid(synonyms):
+    return next((syn for syn in synonyms if syn.startswith("DB")), "Not found")
 
-    base_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{drug_name}/"
+def get_drug_info(drug_names):
+    drugs_data = []
+    for drug_name in drug_names:
+        data = {"SMILES": "Not found", "CID": [], "SID": [], "NSC": [], "DBID": "Not found", "Synonyms": []}
+        base_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{drug_name}/"
 
-    properties = fetch_data(f"{base_url}property/CanonicalSMILES/JSON")
-    data["SMILES"] = (
-        get_property(properties["PropertyTable"]["Properties"][0], "CanonicalSMILES")
-        if properties
-        else "Not found"
-    )
+        properties = fetch_data(f"{base_url}property/CanonicalSMILES/JSON")
+        data["SMILES"] = get_property(properties["PropertyTable"]["Properties"][0], "CanonicalSMILES") if properties else "Not found"
 
-    data["CID"] = get_identifiers(fetch_data(f"{base_url}cids/JSON"), "CID")
-    data["SID"] = get_identifiers(fetch_data(f"{base_url}sids/JSON"), "SID")
+        data["CID"] = get_identifiers(fetch_data(f"{base_url}cids/JSON"), "CID")
+        data["SID"] = get_identifiers(fetch_data(f"{base_url}sids/JSON"), "SID")
 
-    synonyms = fetch_data(f"{base_url}synonyms/JSON")
-    nsc_synonyms = get_synonyms(synonyms, "NSC")
-    data["NSC"] = list(set("".join(filter(str.isdigit, syn)) for syn in nsc_synonyms))
-    data["DBID"] = next((syn for syn in get_synonyms(synonyms, "DB")), "Not found")
+        synonyms = fetch_data(f"{base_url}synonyms/JSON")
+        data["Synonyms"] = get_synonyms(synonyms)
+        data["NSC"] = extract_nsc_numbers(data["Synonyms"])
+        data["DBID"] = extract_dbid(data["Synonyms"])
 
-    return pd.DataFrame([data], index=[drug_name])
+        drugs_data.append(data)
 
+    return pd.DataFrame(drugs_data, index=drug_names)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Retrieve information about a drug from PubChem"
-    )
-    parser.add_argument(
-        "-d", "--drug_name", type=str, help="The name of the drug to query"
-    )
+    parser = argparse.ArgumentParser(description="Retrieve information about drugs from PubChem")
+    parser.add_argument("-d", "--drug_names", nargs='+', type=str, help="The names of the drugs to query")
     args = parser.parse_args()
-    result = get_drug_info(args.drug_name)
+    result = get_drug_info(args.drug_names)
     print(result)
